9b7b22cd1d75fad515221cb4d77f9f31a3ac515a
ascendantaditya <thegokueffect01@gmail.com>
2024-08-01 13:39:40 +0530

    practicequest

---
+++ Loops/h1.java
@@ -0,0 +1,24 @@
+package Loops;
+
+import java.util.*;
+
+public class h1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int i = 1;
+        while (i <= n) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+
+            }
+
+            i++;
+        }
+
+        sc.close();
+
+    }
+
+}
